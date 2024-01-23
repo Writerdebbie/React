4ca263706b34d0bfb61f6a9123a14868dcf5f478
gloria <nnamdidebbie7@gmail.com>
2024-01-23 10:51:45 -0800

    Initial commit

---
+++ react-app/src/App.js
@@ -0,0 +1,23 @@
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <strong>REACT WEATHER APP.</strong> 
+      </header>
+      <Weather />
+      <p>
+        This App is designed by Gloria using React and is Open-sourced on
+        <a href="https://github.com/Writerdebbie"> Github</a> and hosted on {" "}
+        <a href="https://app.netlify.com/teams/writerdebbie/overview">
+          {" "}
+          Netlify
+        </a>{" "}
+      </p>
+    </div>
+  );
+}
+
+export default App;
