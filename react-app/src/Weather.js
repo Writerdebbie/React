24b225955e569c5ed68a17c8102e6216f9722133
gloria <nnamdidebbie7@gmail.com>
2024-01-23 13:32:56 -0800

    Updated spinner loader colors

--- react-app/src/Weather.js
+++ react-app/src/Weather.js
@@ -72,7 +72,7 @@ export default function Weather() {
           ariaLabel="color-ring-loading"
           wrapperStyle={{}}
           wrapperClass="color-ring-wrapper"
-          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          colors={["#e15b64", "#f47e60", "green", "#abbd81", "blueviolet"]}
         />
       </div>
     );
